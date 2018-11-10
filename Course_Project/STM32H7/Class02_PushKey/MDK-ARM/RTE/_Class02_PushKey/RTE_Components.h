d1a6d33b944f846285427a8047cca7b944f6d85f
HaHaHaHaHaGe <823123988@qq.com>
2018-11-10 14:29:17 +0800

    stm32h7_class02_pushkey
    对输入输出的多重模式进行了测试

---
+++ Course_Project/STM32H7/Class02_PushKey/MDK-ARM/RTE/_Class02_PushKey/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Class02_PushKey' 
+ * Target:  'Class02_PushKey' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32h7xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
