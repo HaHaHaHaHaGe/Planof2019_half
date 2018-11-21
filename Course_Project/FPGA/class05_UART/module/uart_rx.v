18c8a0ea5040b8e3f0d7c5305c1e0e615864b4c1
HaHaHaHaHaGe <823123988@qq.com>
2018-11-22 02:25:59 +0800

    fpga_uart(改)
    由于异步系统的亚稳态问题导致RX接收错误，串联两个DQ触发器得到解决
    但是问题仍然存在，在连续发送230个字节左右时会丢失1-2个数据，原因未知

--- Course_Project/FPGA/class05_UART/module/uart_rx.v
+++ Course_Project/FPGA/class05_UART/module/uart_rx.v
@@ -18,10 +18,25 @@ reg flag;
 reg last;
 
 reg begin_bit;
-
+reg rx_1,rx_2;
 
 localparam CHECK_STATE = 1'b0;
 localparam COUNT_STATE = 1'b1;
+
+
+
+
+always@(posedge clk,negedge rst)
+if(!rst) begin
+	rx_1 <= 1'b1;
+	rx_2 <= 1'b1;
+end
+else begin
+	rx_1 <= rx;
+	rx_2 <= rx_1;
+end
+
+
 
 always@(posedge clk,negedge rst) begin
 if(!rst) begin
@@ -30,21 +45,20 @@ if(!rst) begin
 	data <= 8'b0;
 	outdata <=7'b0;
 	flag <= COUNT_STATE;
-	last <= rx;
+	last <= rx_2;
 	cnt2 <= 4'b0;
 	begin_bit <= 1'b0;
-
 end
 else
 case(flag)
 
 COUNT_STATE:
-	if((begin_bit && cnt == 9'd434) || (!begin_bit && last == 1'b1 && rx == 1'b0)) begin
+	if((begin_bit && cnt == 9'd433) || (!begin_bit && last == 1'b1 && rx_2 == 1'b0)) begin
 		flag <= CHECK_STATE;
 		cnt <= 9'b0;
 	end
 	else begin
-		last <= rx;
+		last <= rx_2;
 		irq <= 1'b0;
 		cnt <= cnt + 1'b1;	
 	end
@@ -53,7 +67,7 @@ CHECK_STATE: begin
 	if(!begin_bit) begin
 		begin_bit <= 1'b1;
 		cnt2 <= 1'b1;
-		cnt <= 9'd217;
+		cnt <= 9'd216;
 	end
 	else if(cnt2 == 4'd10) begin
 		begin_bit <= 1'b0;
@@ -62,7 +76,7 @@ CHECK_STATE: begin
 		cnt2 <= 4'b0;
 	end
 	else begin
-		outdata <= {rx,outdata[7:1]};
+		outdata <= {rx_2,outdata[7:1]};
 		cnt2 <= cnt2 + 1'b1;
 	end
 	flag <= COUNT_STATE;
